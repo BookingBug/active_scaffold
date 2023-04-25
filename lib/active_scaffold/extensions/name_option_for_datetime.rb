8b635c7074cce43d0aeb0090c67b9bc8317888eb
Will Leonard <wleonard@jrni.com>
2023-04-25 12:30:33 -0400

    Fix this too

--- lib/active_scaffold/extensions/name_option_for_datetime.rb
+++ lib/active_scaffold/extensions/name_option_for_datetime.rb
@@ -1,15 +1,20 @@
-module ActiveScaffold 
+module ActiveScaffold
   module Extensions
     module NameOptionForDatetime
-      private
+      def self.included(base)
+        base.class_eval do
+          alias_method_chain :datetime_selector, :name
+          private :datetime_selector_without_name, :datetime_selector_with_name, :datetime_selector
+        end
+      end
+
       def datetime_selector_with_name(options, html_options)
         options.merge!(:prefix => options[:name].gsub(/\[[^\[]*\]$/,'')) if options[:name]
         datetime_selector_without_name(options, html_options)        
       end
-      alias_method_chain :datetime_selector, :name
     end
   end
 end
 
 klass = defined?(ActionView::Helpers::InstanceTag) ? ActionView::Helpers::InstanceTag : ActionView::Helpers::Tags::DateSelect
-klass.include(ActiveScaffold::DateSelectExtension)
+klass.include(ActiveScaffold::Extensions::NameOptionForDatetime)
